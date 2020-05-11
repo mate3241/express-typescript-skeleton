7245d5fea24ac784c5120b0f01a2e6ce076379ad
mate3241 <szabomatedenes@gmail.com>
2020-05-11 09:26:05 +0200

    migration

---
+++ migrations/20200511092029_create-flats.js
@@ -0,0 +1,18 @@
+
+exports.up = function (knex) {
+  return knex.schema.createTable('flats', (table) => {
+    table.increments();
+    table.string('title');
+    table.integer('price');
+    table.integer('floorArea');
+    table.string('country');
+    table.integer('zip');
+    table.string('street');
+    table.timestamp('updated_at').defaultTo(knex.raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
+    table.timestamp('created_at').defaultTo(knex.raw('CURRENT_TIMESTAMP'));
+  });
+};
+
+exports.down = function (knex) {
+  return knex.schema.dropTable('flats');
+};
